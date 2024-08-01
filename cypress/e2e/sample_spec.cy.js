d9e57045afe8f1848c19a213d5dd780735a9c6aa
koundinya@k <koundka@amazon.com>
2024-08-01 14:33:57 +0530

    Update sample_spec.cy.js

--- cypress/e2e/sample_spec.cy.js
+++ cypress/e2e/sample_spec.cy.js
@@ -1,6 +1,6 @@
 describe('My First Test', () => {
   it('Visits the React App and fails on purpose', () => {
     cy.visit('/')
-    cy.contains('Non-Existent Text')
+    cy.contains('Learn React')
   })
 })
